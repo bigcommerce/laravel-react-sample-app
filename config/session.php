2ab1fa3c512d9712301227b8920e855536a66e40
Ash Durham <ash@matterdesign.com.au>
2020-11-09 09:14:46 +1100

    Update session settings to allow cross-site cookies

--- config/session.php
+++ config/session.php
@@ -166,7 +166,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', false),
+    'secure' => env('SESSION_SECURE_COOKIE', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -194,6 +194,6 @@ return [
     |
     */
 
-    'same_site' => null,
+    'same_site' => 'none',
 
 ];
